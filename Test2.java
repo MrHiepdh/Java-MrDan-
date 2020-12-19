e9d7cf2d144cd8501657b2f782935d60101b314f
MrHiep01 <nguyencaohiep.ecorp@gmail.com>
2020-12-19 23:46:14 +0700

    week2

---
+++ Test2.java
@@ -0,0 +1,141 @@
+import java.util.*;
+
+public class Test2 {
+    public static void sort(int a[]) {
+        System.out.println("Exercise 1 : ");
+        boolean haveSwap = false;
+        for (int i = 0; i < a.length; i++) {
+            haveSwap = false;
+            for (int j = 0; j < 4 - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+                }
+                haveSwap = true;
+            }
+            if (haveSwap)
+                break;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    public static void sum(int a[]) {
+        System.out.println("Exercise 2 : ");
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        System.out.println(sum);
+    }
+
+    public static void removeDuplicate(int a[]) {
+        int count = 0;
+        sort(a);
+        int b[] = new int[a.length];
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] != a[i + 1]) {
+                b[count++] = a[i];
+            }
+        }
+        b[count] = a[a.length - 1];
+        for (int i = 0; i <= count; i++) {
+            System.out.println(b[i]);
+        }
+    }
+
+    public static void testExist(int a[], int x) {
+        System.out.println("Exercise 4 :");
+        boolean have = false;
+        for (int i = 0; i < a.length && !have; i++) {
+            if (a[i] == x) {
+                have = true;
+            }
+        }
+        if (have) {
+            System.out.println("True!");
+        } else {
+            System.out.println("False!");
+        }
+    }
+
+    public static void findIndex(int a[], int x) {
+        System.out.println("Exercise 5 : ");
+        int index = -1;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == x) {
+                System.out.println(i + " ");
+                index = 1;
+            }
+        }
+        if (index == -1) {
+            System.out.println(x + " isn't in array!");
+        }
+    }
+
+    public static void averageValue(int a[]) {
+        System.out.println("Exercise 3 : ");
+        double average = 0;
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        average = (double) (sum / a.length);
+        System.out.println(average);
+    }
+
+    public static void remove(int a[], int n) {
+        System.out.println("Exercise 6 : ");
+        int i, s = 0;
+        for (i = 0; i < a.length; i++) {
+            if (a[i] == n) {
+                break;
+            }
+        }
+        if (i == a.length) {
+            System.out.println(n + " isn't in array!");
+        } else if (i < a.length) {
+            s = a.length - 1;
+            for (int j = i; j < s; j++) {
+                a[j] = a[j + 1];
+            }
+            for (int j = 0; j < n; j++) {
+                System.out.println(a[j]);
+            }
+        }
+    }
+
+    public static void maxAndMin(int a[]) {
+        System.out.println("Exercise 7 : ");
+        sort(a);
+        System.out.println("Min: " + a[0]);
+        System.out.println("Max: " + a[a.length - 1]);
+    }
+
+    public static void secondLargest(int a[]) {
+        System.out.println("Exercise 9 : ");
+        sort(a);
+        System.out.println(a[a.length - 2]);
+    }
+
+    public static void average2(int a[]) {
+        System.out.println("Exercise 10 : ");
+        double average = 0;
+        sort(a);
+        for (int i = 1; i < a.length - 1; i++) {
+            average += a[i];
+        }
+        average = (double) (average / (a.length - 2));
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Random rd = new Random(); // creating Random object
+        int[] a = new int[10000];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = rd.nextInt(); // storing random integers in an array
+        }
+    }
+}
