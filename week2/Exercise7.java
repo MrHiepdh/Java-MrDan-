f97e73027c54e5fd2af60dcd2817c97d0f1a134a
MrHiep01 <nguyencaohiep.ecorp@gmail.com>
2020-12-25 20:50:25 +0700

    rebuild

--- week2/Exercise7.java
+++ week2/Exercise7.java
@@ -30,30 +30,30 @@ public class Exercise7 {
             a[3][1] -= a[2][1];
             a[2][1] = 0;
         }
-        //Tô màu là 1 cho tất cả các ô thuộc hcn AB
-        for (int i = a[0][0]+1; i <= a[1][0]; i++) {
-        for (int j = a[0][1]+1; j <= a[1][1]; j++) {
-        tmp[i][j] = 1;
-        }
+        // Tô màu là 1 cho tất cả các ô thuộc hcn AB
+        for (int i = a[0][0] + 1; i <= a[1][0]; i++) {
+            for (int j = a[0][1] + 1; j <= a[1][1]; j++) {
+                tmp[i][j] = 1;
+            }
         }
         // Tô màu là 2 cho tất cả các ô thuộc hcn CD
-        for (int i = a[2][0]+1; i <= a[3][0]; i++) {
-        for (int j = a[2][1]+1; j <= a[3][1]; j++) {
-        tmp[i][j] = 2;
-        }
+        for (int i = a[2][0] + 1; i <= a[3][0]; i++) {
+            for (int j = a[2][1] + 1; j <= a[3][1]; j++) {
+                tmp[i][j] = 2;
+            }
         }
         // Đếm số ô được tô 2 trong hcn AB
-        for (int i = a[0][0]+1; i <= a[1][0]; i++) {
-        for (int j = a[0][1]+1; j <= a[1][1]; j++) {
-        if (tmp[i][j] == 2) {
-        count++;
-        }
-        }
+        for (int i = a[0][0] + 1; i <= a[1][0]; i++) {
+            for (int j = a[0][1] + 1; j <= a[1][1]; j++) {
+                if (tmp[i][j] == 2) {
+                    count++;
+                }
+            }
         }
         if (count == 0) {
-        System.out.println("Hai hcn không giao nhau!");
+            System.out.println("Hai hcn không giao nhau!");
         } else {
-        System.out.println("Hai hcn giao nhau " + count + " đơn vị diện tích!");
+            System.out.println("Hai hcn giao nhau " + count + " đơn vị diện tích!");
         }
     }
 
--- week4/Exercise3.java
+++ week4/Exercise3.java
@@ -1,30 +1,24 @@
 package week4;
+
 //idea : sử dụng mảng đánh dấu để xét các kí tự đã đếm
 import java.util.Scanner;
 
 public class Exercise3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int temp[] = new int[26];//khởi tạo 1 mảng có size bằng với số kí tự chữ cái là 26
         String str = sc.nextLine();
         String result = "";
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            int flag = ch;//chuyển từ giá trị kí tự sang giá trị int
-            int count = 1;//biến để đếm số lần xuất hiện
-            if (temp[flag - 97] == 0) {// nếu phần tử với index là giá trị int của kí tự đang xét là 0 thì đếm
-                for (int j = i + 1; j < str.length(); j++) {
-                    if (ch == str.charAt(j))
-                        count++;
-                }
-                if (count > 1) {
-                    String tmp = Integer.toString(count);
-                    result += ch + tmp;
-                } else {
-                    result += ch;
-                }
-                temp[flag - 97] = 1;//gán phần tử hiện tại của mảng là 1 để đánh dấu là đã xét
+        int count = 1;// biến để đếm số lần xuất hiện
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                result += str.charAt(i) + String.valueOf(count);
+                count = 1;
             }
+        }
+        if (str.charAt(str.length() - 1) != str.charAt(str.length() - 2)) {
+            result += str.charAt(str.length() - 1) + "1";
         }
         System.out.println(result);
         sc.close();
