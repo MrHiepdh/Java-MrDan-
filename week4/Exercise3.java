3888b1dab42d6ec0eeb94bcca9768bc56c3d639c
MrHiep01 <nguyencaohiep.ecorp@gmail.com>
2020-12-25 21:51:54 +0700

    Update Exercise3.java

--- week4/Exercise3.java
+++ week4/Exercise3.java
@@ -1,6 +1,4 @@
 package week4;
-
-//idea : sử dụng mảng đánh dấu để xét các kí tự đã đếm
 import java.util.Scanner;
 
 public class Exercise3 {
@@ -12,14 +10,19 @@ public class Exercise3 {
         for (int i = 0; i < str.length() - 1; i++) {
             if (str.charAt(i) == str.charAt(i + 1)) {//nếu bằng tăng biến đếm lên 1
                 count++;
-            } else {// nếu không thì thêm vào chuỗi kết quả và gán lại 1 cho biến đếm để đếm kí tự tiếp theo
+            } else {// nếu không thì thêm vào chuỗi kết quả và reset biến đếm 
+                if(count>1){
                 result += str.charAt(i) + String.valueOf(count);
+                }
+                else if(count==1){
+                    result += str.charAt(i);
+                }
                 count = 1;
             }
         }
-        //vì chỉ xem đến str.length - 1 nên cần xét 2 trường hợp sau
+        //vì chỉ xét đến str.length - 1 nên cần xét 2 trường hợp sau
         if (str.charAt(str.length() - 1) != str.charAt(str.length() - 2)) {
-            result += str.charAt(str.length() - 1) + "1";
+            result += str.charAt(str.length() - 1);
         } else if (str.charAt(str.length() - 1) == str.charAt(str.length() - 2)) {
             result += str.charAt(str.length() - 1) + String.valueOf(count);
         }
