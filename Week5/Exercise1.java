100e6dff086bb85b2b0828c1f4c4949201141fc9
MrHiep01 <nguyencaohiep.ecorp@gmail.com>
2021-01-01 20:03:37 +0700

    a dif version for exercise1

--- Week5/Exercise1.java
+++ Week5/Exercise1.java
@@ -1,6 +1,7 @@
 package Week5;
 
-//idea  : chia số đó thành các bộ 3 chữ số và bắt đầu đọc
+//idea  : chia số đó thành các bộ 3 chữ số và bắt đầu đọc 
+// đọc được các số nhỏ hơn 1000
 import java.util.Scanner;
 
 public class Exercise1 {
@@ -17,7 +18,8 @@ public class Exercise1 {
         if (index1 != 0) {
             read += hundred[index1 - 1] + " trăm ";
         }
-        if (index1 != 0 && index2 == 0 && index3 != 0) {//trường hơp các số 101 đến 109 và tương đương với các chữ số hàng trăm khác
+        if (index1 != 0 && index2 == 0 && index3 != 0) {// trường hơp các số 101 đến 109 và tương đương với các chữ số
+                                                        // hàng trăm khác
             read += "lẻ ";
             if (index3 == 4) {
                 read += "bốn";
@@ -29,7 +31,7 @@ public class Exercise1 {
                 read += unit[index3 - 1];
             }
         } else if (index2 != 0) {
-            if (index2 == 1) {//vì 110 là một trăm mười 
+            if (index2 == 1) {// vì 110 là một trăm mười
                 read += "mười ";
                 if (index3 != 0) {
                     if (index3 == 1) {
@@ -38,7 +40,7 @@ public class Exercise1 {
                         read += unit[index3 - 1];
                     }
                 }
-            } else {//từ 20 thì 0 được đọc là mươi
+            } else {// từ 20 thì 0 được đọc là mươi
                 if (index3 == 0) {
                     read += dozen[index2 - 1] + " mươi";
                 } else {
@@ -60,10 +62,24 @@ public class Exercise1 {
         System.out.println(read);
     }
 
+    public static void solve(int n) {
+        int level = 0;
+        while (n > 0) {
+            level++;
+            n /= 1000;
+        }
+
+        System.out.println(level);
+
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        read(n);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            solve(n);
+        }
         sc.close();
     }
 
---
+++ Week5/Exercise1_2.java
@@ -0,0 +1,51 @@
+package Week5;
+
+//idea : đọc theo các quy tắc thường hay dùng
+//sử dụng hàm replace để chuẩn hóa về các trường hợp đăc biệt
+import java.util.Scanner;
+
+public class Exercise1_2 {
+
+    static String analyze(int n) {
+        if (n == 0)
+            return "không";
+        String[] a = { "tỷ", "trăm", "mươi", "triệu", "trăm", "mươi", "nghìn", "trăm", "mươi" };
+        String[] call = { "không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
+        int i = a.length - 1;
+        String result = "";
+
+        while (n > 0) {
+            result = call[n % 10] + " " + result;
+            if (n >= 10) {
+                result = a[i] + " " + result;
+            }
+            n /= 10;
+            i--;
+        }
+        // replace
+        result = result.replace("mươi năm", "mươi lăm");
+        result = result.replace("mươi bốn", "mươi tư");
+        result = result.replace("mươi một", "mươi mốt");
+        result = result.replace("một mươi", "mười");
+        result = result.replace("không mươi", "linh");
+        result = result.replace("linh lăm", "linh năm");
+        result = result.replace("linh mốt", "linh một");
+        result = result.replace("mười không", "mười");
+        result = result.replace("mười mốt", "mười một");
+        result = result.replace("mười tư", "mười bốn");
+        result = result.replaceAll("không trăm linh không( (" + a[0] + "|" + a[3] + "|" + a[6] + ")|) ", "");
+        result = result.replaceAll("linh không( |)", "");
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int n;
+        while (t-- > 0) {
+            n = sc.nextInt();
+            System.out.println(analyze(n));
+        }
+        sc.close();
+    }
+}
